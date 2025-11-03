9149a0d3681ef0aefcb7387be9abb83e0c5480e3
Mayank Tomar <mayankrana2074@gmail.com>
2025-11-03 15:33:33 +0000

    BMI Index

---
+++ bmi.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    float weight;  
+    printf("Enter weight (in kg) : ");
+    scanf("%f", &weight);
+
+    float height;
+    printf("Enter Height(in meter): ");
+    scanf("%f", &height);
+
+    float BMI = weight/(height*height);
+    printf("Body Mass Index : %f\n", BMI);
+
+    if (BMI < 15)
+        printf("Starvation\n");
+    else if (15.1 <= BMI <= 17.5)
+        printf("Anorexic\n");
+    else if (17.6 <= BMI <= 18.5)
+        printf("Underweight\n");
+    else if (18.6 <= BMI <= 24.9)
+        printf("Ideal\n");
+    else if (BMI <= 25.9)
+        printf("Overweight\n");
+    else if (BMI <= 39.9)
+        printf("Obese\n");
+    else
+        printf("Morbidly Obese\n");
+
+
+    return 0;
+}
