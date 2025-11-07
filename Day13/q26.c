2a755f6d94a0828b19ffb28d3a04dd9b1f2c15f1
Mayank Tomar <mayankrana2074@gmail.com>
2025-11-07 08:17:59 +0000

    Day 17 Completed

--- Day13/q26.c
+++ Day13/q26.c
@@ -5,7 +5,7 @@ int main(){
     printf("Enter n :");
     scanf("%d", &n);
 
-    for(int i = 0; i<=n; i++){
+    for(int i = 1; i<=n; i++){
         printf(" %d ", i);
     }
     printf("\n");
---
+++ Day17/q33.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num, temp, rem, digits = 0;
+    double sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    temp = num;
+    while (temp != 0) {
+        digits++;
+        temp /= 10;
+    }
+
+    temp = num;
+    while (temp != 0) {
+        rem = temp % 10;
+        sum += pow(rem, digits);
+        temp /= 10;
+    }
+
+    if ((int)sum == num)
+        printf("%d is an Armstrong number.\n", num);
+    else
+        printf("%d is not an Armstrong number.\n", num);
+
+    return 0;
+}
