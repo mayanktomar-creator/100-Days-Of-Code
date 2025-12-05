ed43f6c441b35618f4e1e5a362e419d4baeef744
Mayank Tomar <mayankrana2074@gmail.com>
2025-12-05 15:40:18 +0000

    Day 84 Completed

---
+++ Day84/q134.c
@@ -0,0 +1,18 @@
+// Q134: Define an enum with SUCCESS, FAILURE, and TIMEOUT and print messages accordingly.
+
+#include <stdio.h>
+
+enum status {SUCCESS, FAILURE, TIMEOUT};
+
+int main() {
+    enum status s = SUCCESS;
+
+    if (s == SUCCESS)
+        printf("Operation Successful!\n");
+    else if (s == FAILURE)
+        printf("Operation Failed!\n");
+    else
+        printf("Operation Timed Out!\n");
+
+    return 0;
+}
